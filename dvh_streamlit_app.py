c728218fba414dc19640091b7d52bf21ec4578ab
Ivan Macedo <ivanmacedo2010@gmail.com>
2025-10-29 18:18:50 -0300

    fracionamento no final dos dados planilha srs

--- dvh_streamlit_app.py
+++ dvh_streamlit_app.py
@@ -799,13 +799,13 @@ if uploaded_file is not None:
 
         if tipo_tratamento == "SRS (Radiocirurgia)":
             volumes_dict.update({
-                "Fracionamento": n_frações,
                 "Volume >10 Gy (cm³)": volume_10gy,
                 "Volume >12 Gy (cm³)": volume_12gy,
                 "Volume >18 Gy (cm³)": volume_18gy,
                 "Volume >20 Gy (cm³)": volume_20gy,
                 "Volume >25 Gy (cm³)": volume_25gy,
                 "Volume >30 Gy (cm³)": volume_30gy,
+                "Fracionamento": n_frações,
             })
 
         elif tipo_tratamento == "SBRT de Pulmão":
@@ -821,6 +821,7 @@ if uploaded_file is not None:
 
 else:
     st.info("Por favor, selecione o tipo de tratamento na barra lateral. Em seguida, envie um arquivo .txt de DVH tabulado em Upload do Arquivo para iniciar a análise. O DVH tabulado precisa ser de um gráfico cumulativo, com dose absoluta e volume absoluto, contendo, no mínimo, as estruturas de Corpo, PTV, Interseção entre o PTV e a Isodose de Prescrição, e Isodose de 50%. Para o caso de SBRT de Pulmão, também é necessário uma estrutura para o Pulmão a ser avaliado o V20Gy.")
+
 
 
 
